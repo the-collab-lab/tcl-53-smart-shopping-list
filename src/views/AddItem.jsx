a10426796703bf630f2d1d0ddccc189ef5aeb817
Nina Fuentes <nina.fuentes.n@gmail.com>
2023-01-15 13:26:17 -0500

    added current issues as a comment at bottom of page

--- src/views/AddItem.jsx
+++ src/views/AddItem.jsx
@@ -1,21 +1,29 @@
-export function AddItem() {
+import { addItem } from '../api/firebase';
+import { List } from './List';
+
+// addItem(listId, { itemName, daysUntilNextPurchase })
+
+export function AddItem({ listToken }) {
 	return (
 		<>
 			{/* <p>Add Item</p> */}
-			<label htmlFor="itemName">Item Name</label>
-			<input name="itemName"></input>
+			{/* <form onSubmit = {addItem(listToken, { itemName, })}> */}
+			<form>
+				<label htmlFor="itemName">Item Name</label>
+				<input name="itemName"></input>
 
-			<label htmlFor="buyAgain">How soon will you buy this again?</label>
-			<fieldset name="buyAgain">
-				<label htmlFor="soon">Soon</label>
-				<input type="radio" name="soon" value={7} checked></input>
+				<label htmlFor="buyAgain">How soon will you buy this again?</label>
+				<fieldset name="buyAgain">
+					<label htmlFor="soon">Soon</label>
+					<input type="radio" name="soon" value={7} checked></input>
 
-				<label htmlFor="kindaSoon">Kinda Soon</label>
-				<input type="radio" name="kindaSoon" value={14}></input>
+					<label htmlFor="kindaSoon">Kinda Soon</label>
+					<input type="radio" name="kindaSoon" value={14}></input>
 
-				<label htmlFor="notSoon">Not Soon</label>
-				<input type="radio" name="notSoon" value={30}></input>
-			</fieldset>
+					<label htmlFor="notSoon">Not Soon</label>
+					<input type="radio" name="notSoon" value={30}></input>
+				</fieldset>
+			</form>
 
 			<button>Add Item</button>
 		</>
@@ -24,3 +32,12 @@ export function AddItem() {
 	// 	Hello from the <code>/add-item</code> page!
 	// 	</p>
 }
+
+/**
+ * Issues:
+ * select only 1 radio button
+ * how to setup the function for on submit
+ * get the button to submit the form
+ * submit the form when pressing "Enter/Return"
+ * how to get the listID
+ */
