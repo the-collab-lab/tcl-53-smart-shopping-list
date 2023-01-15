349064a036458314dac80fe5e2a005a3d5185392
Nina Fuentes <nina.fuentes.n@gmail.com>
2023-01-15 12:41:13 -0500

    placed initial elements on the page

--- src/views/AddItem.jsx
+++ src/views/AddItem.jsx
@@ -1,3 +1,26 @@
 export function AddItem() {
-	return <p>Hello from the <code>/add-item</code> page!</p>
+	return (
+		<>
+			{/* <p>Add Item</p> */}
+			<label htmlFor="itemName">Item Name</label>
+			<input name="itemName"></input>
+
+			<label htmlFor="buyAgain">How soon will you buy this again?</label>
+			<fieldset name="buyAgain">
+				<label htmlFor="soon">Soon</label>
+				<input type="radio" name="soon" value={7} checked></input>
+
+				<label htmlFor="kindaSoon">Kinda Soon</label>
+				<input type="radio" name="kindaSoon" value={14}></input>
+
+				<label htmlFor="notSoon">Not Soon</label>
+				<input type="radio" name="notSoon" value={30}></input>
+			</fieldset>
+
+			<button>Add Item</button>
+		</>
+	);
+	// <p>
+	// 	Hello from the <code>/add-item</code> page!
+	// 	</p>
 }
