15969952c889dae7a44db7d2964eff2b86fb86f0
Nina Fuentes <nina.fuentes.n@gmail.com>
2023-01-15 14:11:58 -0500

    got the radio buttons to work as intended and the form to submit when pressing button or ENTER

--- src/views/AddItem.jsx
+++ src/views/AddItem.jsx
@@ -1,31 +1,79 @@
-import { addItem } from '../api/firebase';
 import { List } from './List';
+
+import { useState } from 'react';
+import { addItem } from '../api/firebase';
 
 // addItem(listId, { itemName, daysUntilNextPurchase })
 
 export function AddItem({ listToken }) {
+	const soon = '7';
+	const kindaSoon = '14';
+	const notSoon = '30';
+
+	const [nextPurchase, setNextPurchase] = useState(soon);
+
+	const handleChange = (e) => {
+		setNextPurchase(e.target.value);
+	};
+
+	const submitForm = (e) => {
+		e.preventDefault();
+
+		//define listID
+		//get itemName
+		console.log('form submitted');
+
+		let daysUntilNextPurchase = Number(nextPurchase);
+
+		// addItem(listId, { itemName, daysUntilNextPurchase });
+	};
+
 	return (
 		<>
 			{/* <p>Add Item</p> */}
 			{/* <form onSubmit = {addItem(listToken, { itemName, })}> */}
-			<form>
+			<form onSubmit={submitForm}>
 				<label htmlFor="itemName">Item Name</label>
-				<input name="itemName"></input>
+				<input id="itemName"></input>
 
 				<label htmlFor="buyAgain">How soon will you buy this again?</label>
 				<fieldset name="buyAgain">
-					<label htmlFor="soon">Soon</label>
-					<input type="radio" name="soon" value={7} checked></input>
+					<label htmlFor="soon">
+						<input
+							type="radio"
+							id="soon"
+							value={soon}
+							checked={nextPurchase === soon}
+							onChange={handleChange}
+						/>
+						Soon
+					</label>
 
-					<label htmlFor="kindaSoon">Kinda Soon</label>
-					<input type="radio" name="kindaSoon" value={14}></input>
+					<label htmlFor="kindaSoon">
+						<input
+							type="radio"
+							id="kindaSoon"
+							value={kindaSoon}
+							checked={nextPurchase === kindaSoon}
+							onChange={handleChange}
+						/>
+						Kinda Soon
+					</label>
 
-					<label htmlFor="notSoon">Not Soon</label>
-					<input type="radio" name="notSoon" value={30}></input>
+					<label htmlFor="notSoon">
+						<input
+							type="radio"
+							id="notSoon"
+							value={notSoon}
+							checked={nextPurchase === notSoon}
+							onChange={handleChange}
+						/>
+						Not Soon
+					</label>
 				</fieldset>
-			</form>
 
-			<button>Add Item</button>
+				<button type="submit">Add Item</button>
+			</form>
 		</>
 	);
 	// <p>
@@ -35,9 +83,9 @@ export function AddItem({ listToken }) {
 
 /**
  * Issues:
- * select only 1 radio button
+ * select only 1 radio button [DONE]
  * how to setup the function for on submit
  * get the button to submit the form
- * submit the form when pressing "Enter/Return"
+ * submit the form when pressing ""
  * how to get the listID
  */
