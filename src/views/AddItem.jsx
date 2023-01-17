e06400952e68699f1a78fcc59b05d1341c568d0a
Nina Fuentes <nina.fuentes.n@gmail.com>
2023-01-17 21:33:37 +0100

    implemented suggested code changes and re-introduced necessary items

--- src/views/AddItem.jsx
+++ src/views/AddItem.jsx
@@ -2,64 +2,109 @@ import { useState } from 'react';
 import { addItem } from '../api/firebase';
 
 export function AddItem({ listToken }) {
-	const [itemName, setItemName] = useState('');
-	const [nextPurchase, setNextPurchase] = useState(0);
-	const [submissionConfirmation, setSubmissionConfirmation] = useState('');
+	const soon = '7';
+	const kindaSoon = '14';
+	const notSoon = '30';
 
-	const handleChange = (e) => {
+	const [itemName, setItemName] = useState('');
+	const [nextPurchase, setNextPurchase] = useState(soon);
+	const [submissionConfirmation, setsubmissionConfirmation] = useState('');
+
+	const handleChangeItem = (e) => {
 		setItemName(e.target.value);
+	};
+
+	const handleChangeNextPurchase = (e) => {
+		setNextPurchase(e.target.value);
 	};
 
 	const submitForm = (e) => {
 		e.preventDefault();
-		try {
-			addItem(listToken, { itemName, daysUntilNextPurchase: nextPurchase });
-			// TO-DO: add useEffect to show successful and unsuccessful submits (failed, success)
-		} catch {
-			alert('There was a problem adding your item, please try again.');
+
+		//form validation
+		if (itemName === '') {
+			alert('Please specify the name of the item');
+			return;
+		}
+
+		let itemData = {
+			itemName: itemName,
+			daysUntilNextPurchase: Number(nextPurchase),
+		};
+
+		// additem to the database
+		const submission = addItem(listToken, itemData);
+
+		// (if submission was sucessful successful)
+		if (submission) {
+			//clear form
+			setItemName('');
+			setNextPurchase(soon);
+
+			// put a note that the form was submitted
+			setsubmissionConfirmation(`${itemName} was added to your shopping list.`);
+			setTimeout(setsubmissionConfirmation, 10000);
+		} else {
+			// notify that the item was not submitted
+			setsubmissionConfirmation(
+				`There was a problem adding your item, please try again.`,
+			);
 		}
 	};
 
 	return (
-		<main>
-			<form>
-				<p>Item Name</p>
-				<input
-					type="text"
-					id="itemName"
-					placeholder="Item Name"
-					onChange={handleChange}
-				/>{' '}
-				<br />
-				<input
-					type="radio"
-					id="soon"
-					name="purchase-frequency"
-					onChange={(e) => setNextPurchase(7)}
-				/>
-				<label htmlFor="soon">Soon</label>
-				<br />
-				<input
-					type="radio"
-					id="kind-of-soon"
-					name="purchase-frequency"
-					onChange={(e) => setNextPurchase(14)}
-				/>
-				<label htmlFor="kind-of-soon">Kind of soon</label>
-				<br />
-				<input
-					type="radio"
-					id="not-soon"
-					name="purchase-frequency"
-					onChange={(e) => setNextPurchase(30)}
-				/>
-				<label htmlFor="not-soon">Not soon</label>
-				<p>
-					<button type="submit" onClick={submitForm}>
-						Add Item
-					</button>
-				</p>
+		<>
+			<form onSubmit={submitForm}>
+				<label htmlFor="itemName">
+					Item Name
+					<input
+						type="text"
+						id="itemName"
+						value={itemName}
+						onChange={handleChangeItem}
+					/>
+				</label>
+
+				<label htmlFor="buyAgain">How soon will you buy this again?</label>
+				<fieldset name="buyAgain">
+					<label htmlFor="soon">
+						<input
+							type="radio"
+							id="soon"
+							value={soon}
+							checked={nextPurchase === soon}
+							onChange={handleChangeNextPurchase}
+						/>
+						Soon
+					</label>
+
+					<label htmlFor="kindaSoon">
+						<input
+							type="radio"
+							id="kindaSoon"
+							value={kindaSoon}
+							checked={nextPurchase === kindaSoon}
+							onChange={handleChangeNextPurchase}
+						/>
+						Kinda Soon
+					</label>
+
+					<label htmlFor="notSoon">
+						<input
+							type="radio"
+							id="notSoon"
+							value={notSoon}
+							checked={nextPurchase === notSoon}
+							onChange={handleChangeNextPurchase}
+						/>
+						Not Soon
+					</label>
+				</fieldset>
+
+				<button type="submit">Add Item</button>
 			</form>
-		</main>
+
+			<p>{submissionConfirmation}</p>
+		</>
 	);
 }
