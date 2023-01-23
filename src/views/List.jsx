f6b32589aab7e3adaa09b7ca11ba1a2832c8a0ac
Nina Fuentes <nina.fuentes.n@gmail.com>
2023-01-23 19:54:00 +0100

    added search box to list view to filter items

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -1,16 +1,41 @@
+import React, { useState } from 'react';
 import { ListItem } from '../components';
 
 export function List({ data }) {
+	const [searchedItem, setSearchedItem] = useState('');
+
+	function handleChange(e) {
+		setSearchedItem(e.target.value);
+	}
+
+	const filteredItems = data.filter((item) =>
+		item.name.toLowerCase().includes(searchedItem.toLowerCase()),
+	);
+
+	console.log(filteredItems);
 	return (
 		<>
 			<p>
 				Hello from the <code>/list</code> page!
 			</p>
+			<form>
+				<label htmlFor="search">Filter items</label>
+				<input
+					id="search"
+					type="search"
+					placeholder="Start typing here..."
+					value={searchedItem}
+					onChange={handleChange}
+				/>
+			</form>
 			<ul>
-				{/* Add field to filter list */}
-				{data.map((list) => {
-					return <ListItem name={list.name} key={list.id} />;
-				})}
+				{!filteredItems.length ? (
+					<p>no item found</p>
+				) : (
+					filteredItems.map((list) => {
+						return <ListItem name={list.name} key={list.id} />;
+					})
+				)}
 			</ul>
 		</>
 	);
