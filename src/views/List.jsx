858d1729be00e023c320bf3b159c4a62bf33274c
Jeff <herrbourne@gmail.com>
2023-01-30 10:45:04 -0800

    Revert "Merge branch jly-jla-share-list to main"
    This reverts commit 3ca4202901426efa2cdf08a7baa12405fe9afa53.

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { ListItem } from '../components';
-import { useNavigate } from 'react-router-dom';
 
 export function List({ data }) {
 	const [searchedItem, setSearchedItem] = useState('');
@@ -12,12 +11,6 @@ export function List({ data }) {
 	const filteredItems = data.filter((item) =>
 		item.name.toLowerCase().includes(searchedItem.toLowerCase()),
 	);
-
-	let navigate = useNavigate();
-	const routeToAddItem = () => {
-		let path = `/add-item`;
-		navigate(path);
-	};
 
 	return (
 		<>
@@ -42,11 +35,7 @@ export function List({ data }) {
 			</form>
 			<ul>
 				{!filteredItems.length ? (
-					<div>
-						<p>Your shopping list is currently empty.</p>
-						<a href="/add-item">Add your first item</a>
-						<button onClick={routeToAddItem}>Add your first item</button>
-					</div>
+					<p>No items found.</p>
 				) : (
 					filteredItems.map((list) => {
 						return <ListItem name={list.name} key={list.id} />;
