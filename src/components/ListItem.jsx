2dee49b286d8f29f3085ccee6b5ddbe3e6b93e76
Nina Fuentes <nina.fuentes.n@gmail.com>
2023-02-01 13:28:24 -0500

    ensured that item stays checked in a period of 24hrs

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -6,17 +6,23 @@ import { getFutureDate } from '../utils';
 export function ListItem({ name, listToken, itemId, data }) {
 	const [checkedState, setCheckedState] = useState(data.checked);
 
+	const checkTimePast = () => {
+		const yesterday = getFutureDate(-1);
+		const dateLastPurchased = new Date(data.dateLastPurchased.toDate());
+
+		return yesterday > dateLastPurchased;
+	};
+
 	const handleChange = () => {
-		setCheckedState(!data.checked);
-		updateItem(listToken, itemId, !data.checked);
+		if (checkTimePast() || data.checked === false) {
+			setCheckedState(!data.checked);
+			updateItem(listToken, itemId, !data.checked);
+		}
 	};
 
 	useEffect(function unCheckAfterTime() {
 		if (data.dateLastPurchased !== null) {
-			const yesterday = getFutureDate(-1);
-			const dateLastPurchased = new Date(data.dateLastPurchased.toDate());
-
-			if (yesterday > dateLastPurchased && data.checked === true) {
+			if (checkTimePast() && data.checked === true) {
 				setCheckedState(!data.checked);
 				updateItem(listToken, itemId, !data.checked);
 			}
