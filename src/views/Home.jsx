a9e301f22fc6302d9a4830b0a26bbd011fe20358
Nesha Mervin <nesha.mervin@gmail.com>
2023-02-28 21:07:41 -0500

    styled home page, adding logo, app name and slogan, buttons and background image

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,8 +1,9 @@
-import './Home.css';
-import { generateToken } from '@the-collab-lab/shopping-list-utils';
-import { useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { generateToken } from '@the-collab-lab/shopping-list-utils';
 import { getItemData, streamListItems } from '../api';
+
+import './Home.css';
 
 export function Home({ listToken, setListToken }) {
 	const [existingToken, setExistingToken] = useState('');
@@ -43,32 +44,46 @@ export function Home({ listToken, setListToken }) {
 			}
 		});
 	};
+
 	return (
-		<div className="Home">
-			<p>
-				Hello from the home (<code>/</code>) page!
-			</p>
+		<div className="Home h-screen flex flex-col items-center justify-around pt-8">
+			<div className="flex flex-col items-center gap-6">
+				<img src="/img/logo.svg" alt="Shroomy logo" className="w-20 mt-6" />
+				<h1 className="text-6xl font-logo -my-2">Shroomy</h1>
+				<p className="text-2xl font-logo">your groovy shopping companion</p>
 
-			<div>
-				<button onClick={createToken}>Create a new list</button>
+				<div className="flex flex-col items-center gap-6 mt-6">
+					<button
+						onClick={createToken}
+						className="bg-main-darkest text-white py-2 px-12 rounded-3xl shadow-lg hover:bg-white hover:text-main-darkest hover:border-main-darkest hover:border-[1.5px]"
+					>
+						Create a new list
+					</button>
 
-				<form onSubmit={handleSubmit}>
-					<label htmlFor="existingToken">
-						Join an existing list
-						<input
-							type="text"
-							id="existingToken"
-							value={existingToken}
-							onChange={handleChange}
-							required
-						/>
-					</label>
+					<button className="bg-white text-main-darkest py-2 px-12 border-[1.5px] border-main-darkest rounded-3xl shadow-lg hover:bg-main-darkest hover:text-white">
+						Join Existing List
+					</button>
 
-					<button type="submit">Submit</button>
-				</form>
+					<form onSubmit={handleSubmit}>
+						<label htmlFor="existingToken">
+							Join an existing list
+							<input
+								type="text"
+								id="existingToken"
+								value={existingToken}
+								onChange={handleChange}
+								required
+							/>
+						</label>
 
-				{errorMessage && <p>{errorMessage}</p>}
+						<button type="submit">Submit</button>
+					</form>
+
+					{errorMessage && <p>{errorMessage}</p>}
+				</div>
 			</div>
+
+			<div className="bg-[url('/img/home-bg.svg')] w-full h-64 mb-6"></div>
 		</div>
 	);
 }
