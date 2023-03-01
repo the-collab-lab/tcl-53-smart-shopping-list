e62548cad38d0ae64e9b1cec12ecaa3d07516276
Nesha Mervin <nesha.mervin@gmail.com>
2023-02-28 21:37:20 -0500

    set up functionality for opening and closing modal and created portal for rendering form modal into a separate div on top of the Home component

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,13 +1,16 @@
 import { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
 import { useNavigate } from 'react-router-dom';
 import { generateToken } from '@the-collab-lab/shopping-list-utils';
 import { getItemData, streamListItems } from '../api';
+import FormModal from '../components/FormModal';
 
 import './Home.css';
 
 export function Home({ listToken, setListToken }) {
 	const [existingToken, setExistingToken] = useState('');
 	const [errorMessage, setErrorMessage] = useState('');
+	const [displayForm, setDisplayForm] = useState(false);
 
 	const navigate = useNavigate();
 
@@ -45,8 +48,16 @@ export function Home({ listToken, setListToken }) {
 		});
 	};
 
+	function openForm() {
+		setDisplayForm(true);
+	}
+
+	function closeForm() {
+		setDisplayForm(false);
+	}
+
 	return (
-		<div className="Home h-screen flex flex-col items-center justify-around pt-8">
+		<div className="h-screen flex flex-col items-center justify-around pt-8">
 			<div className="flex flex-col items-center gap-6">
 				<img src="/img/logo.svg" alt="Shroomy logo" className="w-20 mt-6" />
 				<h1 className="text-6xl font-logo -my-2">Shroomy</h1>
@@ -55,31 +66,29 @@ export function Home({ listToken, setListToken }) {
 				<div className="flex flex-col items-center gap-6 mt-6">
 					<button
 						onClick={createToken}
-						className="bg-main-darkest text-white py-2 px-12 rounded-3xl shadow-lg hover:bg-white hover:text-main-darkest hover:border-main-darkest hover:border-[1.5px]"
+						className="bg-main-darkest text-white rounded-3xl shadow-lg py-2 px-12 hover:bg-white hover:text-main-darkest hover:border-main-darkest hover:border-[1.5px]"
 					>
 						Create a new list
 					</button>
 
-					<button className="bg-white text-main-darkest py-2 px-12 border-[1.5px] border-main-darkest rounded-3xl shadow-lg hover:bg-main-darkest hover:text-white">
+					<button
+						className="bg-white text-main-darkest border-[1.5px] border-main-darkest rounded-3xl shadow-lg py-2 px-12 hover:bg-main-darkest hover:text-white"
+						onClick={openForm}
+					>
 						Join Existing List
 					</button>
 
-					<form onSubmit={handleSubmit}>
-						<label htmlFor="existingToken">
-							Join an existing list
-							<input
-								type="text"
-								id="existingToken"
-								value={existingToken}
-								onChange={handleChange}
-								required
-							/>
-						</label>
-
-						<button type="submit">Submit</button>
-					</form>
-
-					{errorMessage && <p>{errorMessage}</p>}
+					{displayForm &&
+						ReactDOM.createPortal(
+							<FormModal
+								handleSubmit={handleSubmit}
+								existingToken={existingToken}
+								handleChange={handleChange}
+								closeForm={closeForm}
+								errorMessage={errorMessage}
+							/>,
+							document.getElementById('overlay-root'),
+						)}
 				</div>
 			</div>
 
