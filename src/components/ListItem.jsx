bd37460e461b0f0ad4947a27ce52506d5f69440a
Nesha Mervin <nesha.mervin@gmail.com>
2023-03-03 18:59:42 -0500

    rendered detailed list if allDetailsOpen is true and enabled toggling functionality with opening and closing list details

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -17,9 +17,11 @@ export function ListItem({
 	urgency,
 	listLength,
 	index,
+	allDetailsOpen,
 }) {
 	const [checkedState, setCheckedState] = useState(data.checked);
 	const [detailsOpen, setDetailsOpen] = useState(false);
+	const [detailsClose, setDetailsClose] = useState(false);
 
 	const urgencyString = (urgency) => {
 		if (urgency === 0) {
@@ -111,9 +113,95 @@ export function ListItem({
 
 	return (
 		<>
-			<li
-				style={{ zIndex: String(listLength - index - 1) }}
-				className={`w-full text-white relative px-4
+			{allDetailsOpen && !detailsClose ? (
+				<li
+					style={{ zIndex: String(listLength - index - 1) }}
+					className={`w-full text-white relative px-4
+				${urgencyColors[urgency]}
+				${
+					listLength === 1
+						? listItemStyles['unique']
+						: index === 0
+						? listItemStyles['top-style']
+						: index === listLength - 1
+						? listItemStyles['bottom-style']
+						: listItemStyles['middle-style']
+				}`}
+				>
+					{' '}
+					<div className="flex justify-between align-center">
+						<label htmlFor={name} className="flex">
+							<input
+								type="checkbox"
+								id={name}
+								onChange={handleChange}
+								checked={checkedState}
+								className="hidden"
+							/>
+							{data.checked && (
+								<FontAwesomeIcon
+									icon={faCircleCheck}
+									className="mr-2 text-xl self-center"
+								/>
+							)}
+							{!data.checked && (
+								<FontAwesomeIcon
+									icon={faCircle}
+									className="mr-2 hover:cursor-pointer text-xl self-center"
+								/>
+							)}
+							<span className="mr-2 hover:cursor-pointer self-center">
+								{name}
+							</span>
+							<img
+								src={urgencyMushroomsImg[urgency]}
+								className="inline-block h-7 self-center"
+							/>
+						</label>
+
+						<div>
+							<button onClick={() => setDetailsClose(true)}>
+								{!detailsClose && (
+									<FontAwesomeIcon
+										icon={faCircleChevronUp}
+										className="text-xl"
+									/>
+								)}
+
+								{detailsClose && (
+									<FontAwesomeIcon
+										icon={faCircleChevronDown}
+										className="text-xl"
+										onClick={() => setDetailsOpen(true)}
+									/>
+								)}
+							</button>
+
+							<button onClick={confirmDelete} className="ml-5 text-xl">
+								<FontAwesomeIcon icon={faTrashCan} />
+							</button>
+						</div>
+					</div>
+					{!detailsClose && (
+						<div className="flex flex-col justify-between max-w-xs mt-3 ml-7 text-sm">
+							<p>
+								Next Purchase: <span className="font-bold">{nextPurchase}</span>
+								{` (${urgencyString(urgency)})`}
+							</p>
+							<p>
+								Last Purchase: <span className="">{lastPurchase}</span>
+							</p>
+							<p>
+								Total Purchases:{' '}
+								<span className="font-bold">{data.totalPurchases}</span>
+							</p>
+						</div>
+					)}
+				</li>
+			) : (
+				<li
+					style={{ zIndex: String(listLength - index - 1) }}
+					className={`w-full text-white relative px-4
 					${urgencyColors[urgency]}
 					${
 						listLength === 1
@@ -124,73 +212,77 @@ export function ListItem({
 							? listItemStyles['bottom-style']
 							: listItemStyles['middle-style']
 					}`}
-			>
-				<div className="flex justify-between align-center">
-					<label htmlFor={name} className="flex">
-						<input
-							type="checkbox"
-							id={name}
-							onChange={handleChange}
-							checked={checkedState}
-							className="hidden"
-						/>
-						{data.checked && (
-							<FontAwesomeIcon
-								icon={faCircleCheck}
-								className="mr-2 text-xl self-center"
+				>
+					<div className="flex justify-between align-center">
+						<label htmlFor={name} className="flex">
+							<input
+								type="checkbox"
+								id={name}
+								onChange={handleChange}
+								checked={checkedState}
+								className="hidden"
 							/>
-						)}
-						{!data.checked && (
-							<FontAwesomeIcon
-								icon={faCircle}
-								className="mr-2 hover:cursor-pointer text-xl self-center"
-							/>
-						)}
-						<span className="mr-2 hover:cursor-pointer self-center">
-							{name}
-						</span>
-						<img
-							src={urgencyMushroomsImg[urgency]}
-							className="inline-block h-7 self-center"
-						/>
-					</label>
-
-					<div>
-						<button onClick={() => setDetailsOpen(!detailsOpen)}>
-							{detailsOpen && (
-								<FontAwesomeIcon icon={faCircleChevronUp} className="text-xl" />
-							)}
-
-							{!detailsOpen && (
+							{data.checked && (
 								<FontAwesomeIcon
-									icon={faCircleChevronDown}
-									className="text-xl"
+									icon={faCircleCheck}
+									className="mr-2 text-xl self-center"
 								/>
 							)}
-						</button>
+							{!data.checked && (
+								<FontAwesomeIcon
+									icon={faCircle}
+									className="mr-2 hover:cursor-pointer text-xl self-center"
+								/>
+							)}
+							<span className="mr-2 hover:cursor-pointer self-center">
+								{name}
+							</span>
+							<img
+								src={urgencyMushroomsImg[urgency]}
+								className="inline-block h-7 self-center"
+							/>
+						</label>
 
-						<button onClick={confirmDelete} className="ml-5 text-xl">
-							<FontAwesomeIcon icon={faTrashCan} />
-						</button>
-					</div>
-				</div>
+						<div>
+							<button onClick={() => setDetailsOpen(!detailsOpen)}>
+								{detailsOpen && (
+									<FontAwesomeIcon
+										icon={faCircleChevronUp}
+										className="text-xl"
+									/>
+								)}
 
-				{detailsOpen && (
-					<div className="flex flex-col justify-between max-w-xs mt-3 ml-7 text-sm">
-						<p>
-							Next Purchase: <span className="font-bold">{nextPurchase}</span>
-							{` (${urgencyString(urgency)})`}
-						</p>
-						<p>
-							Last Purchase: <span className="">{lastPurchase}</span>
-						</p>
-						<p>
-							Total Purchases:{' '}
-							<span className="font-bold">{data.totalPurchases}</span>
-						</p>
+								{!detailsOpen && (
+									<FontAwesomeIcon
+										icon={faCircleChevronDown}
+										className="text-xl"
+									/>
+								)}
+							</button>
+
+							<button onClick={confirmDelete} className="ml-5 text-xl">
+								<FontAwesomeIcon icon={faTrashCan} />
+							</button>
+						</div>
 					</div>
-				)}
-			</li>
+
+					{detailsOpen && (
+						<div className="flex flex-col justify-between max-w-xs mt-3 ml-7 text-sm">
+							<p>
+								Next Purchase: <span className="font-bold">{nextPurchase}</span>
+								{` (${urgencyString(urgency)})`}
+							</p>
+							<p>
+								Last Purchase: <span className="">{lastPurchase}</span>
+							</p>
+							<p>
+								Total Purchases:{' '}
+								<span className="font-bold">{data.totalPurchases}</span>
+							</p>
+						</div>
+					)}
+				</li>
+			)}
 		</>
 	);
 }
